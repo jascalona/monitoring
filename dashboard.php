ab600c1373f4357ff86617164f1de751fc6f06c2
jaescalona <abrahamjose.1587@gmail.com>
2024-06-17 16:53:15 -0400

    new

--- dashboard.php
+++ dashboard.php
@@ -34,7 +34,7 @@ if (!empty($_SESSION["CI"])) {
 <header class="header">
         
         <div class="logo">
-            <p>XDV</p>
+            <a href="./panel.php"><p>XDV</p></a>
 
         <div class="btn-sesion">
             <div class="btn-group" role="group">
@@ -69,37 +69,7 @@ if (!empty($_SESSION["CI"])) {
 
 
 
-    <div class="carga-modal">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Open modal for @mdo</button>
-    
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form>
-                <div class="mb-3">
-                    <label for="recipient-name" class="col-form-label">Recipient:</label>
-                    <input type="text" class="form-control" id="recipient-name">
-                </div>
-                <div class="mb-3">
-                    <label for="message-text" class="col-form-label">Message:</label>
-                    <textarea class="form-control" id="message-text"></textarea>
-                </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Send message</button>
-            </div>
-            </div>
-        </div>
-        </div>
-    </div>
-
+   
 
 
 
