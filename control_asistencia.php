81a077872e978ee9d4fedcc60ab075aab2abab4a
jaescalona <abrahamjose.1587@gmail.com>
2024-06-17 17:11:36 -0400

    new

--- control_asistencia.php
+++ control_asistencia.php
@@ -102,6 +102,37 @@ if (!empty($_SESSION["CI"])) {
 
 
 
+    <div class="container-tables">
+        <table class="table table-striped">
+        <thead>
+            <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Apellido</th>
+            <th scope="col">CI</th>
+            <th scope="col">Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
 
 
         
