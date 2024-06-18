f144dd850ce898172cb9e3e503a08d1ce7b43e0c
jaescalona <abrahamjose.1587@gmail.com>
2024-06-18 15:53:20 -0400

    new

--- control_asistencia.php
+++ control_asistencia.php
@@ -35,115 +35,115 @@ include "./CONTROLLER/conexion.php";
 <body>
 
 
-<header class="header">
+    <header class="header">
+            
+            <div class="logo">
+                <p>XDV</p>
+
+            <div class="btn-sesion">
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    
+                    <?php
+                    echo $_SESSION["name"];
+                    ?>
+
+                    </button>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="./CONTROLLER//close.php">Cerrar Sesion</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         
-        <div class="logo">
-            <p>XDV</p>
 
-        <div class="btn-sesion">
-            <div class="btn-group" role="group">
-                <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                
-                <?php
-                echo $_SESSION["name"];
-                ?>
 
-                </button>
-                <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="./CONTROLLER//close.php">Cerrar Sesion</a></li>
+            <nav>
+                <ul class="nav-link">
+                    <li><a href="./panel.php"><i style="color: #1EE08E;" class='bx bxs-dashboard'></i>Dashboard</a></li>
+                    <li><a href="./control_asistencia.php"><i style="color: #f60551;" class='bx bx-task'></i>Control de Asistencias</a></li>
+                    <li><a href="control_cuentas.php"><i style="color: #09bfdf;" class='bx bxs-contact'></i>Control de Cuentas</a></li>
+                    <li><a href=""><i style="color: #2F35AF;" class='bx bxs-server' ></i>Registros</a></li>
+                    <li></li>
                 </ul>
-            </div>
-        </div>
-    </div>
-    
+            
+            </nav>
+        </header>
 
 
-        <nav>
-            <ul class="nav-link">
-                <li><a href="./panel.php"><i style="color: #1EE08E;" class='bx bxs-dashboard'></i>Dashboard</a></li>
-                <li><a href="./control_asistencia.php"><i style="color: #f60551;" class='bx bx-task'></i>Control de Asistencias</a></li>
-                <li><a href=""><i style="color: #09bfdf;" class='bx bxs-contact'></i>Control de Cuentas</a></li>
-                <li><a href=""><i style="color: #2F35AF;" class='bx bxs-server' ></i>Registros</a></li>
-                <li></li>
-            </ul>
+
+
+        <div class="carga-modal">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Cargar Asistencia</button>
         
-        </nav>
-    </header>
-
-
-
-
-    <div class="carga-modal">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Cargar Asistencia</button>
-    
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Control de Asistencias</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form>
-                
-                <div class="mb-3">
-                    <label for="recipient-name" class="col-form-label">CI</label>
-                    <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Documento">
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Control de Asistencias</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
+                <div class="modal-body">
+                    <form>
+                    
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">CI</label>
+                        <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Documento">
+                    </div>
 
 
-                <div class="mb-3">
-                    <label for="recipient-name" class="col-form-label">Nombre</label>
-                    <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Nombre">
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Nombre</label>
+                        <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Nombre">
+                    </div>
+
+
+                    <!--
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Message:</label>
+                        <textarea class="form-control" id="message-text"></textarea>
+                    </div>
+                    -->
+
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Apellido</label>
+                        <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Apellido">
+                    </div>
+
+
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Email</label>
+                        <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Email">
+                    </div>
+
+
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Fecha</label>
+                        <input type="text" class="form-control" id="recipient-name" placeholder="Fecha de Ingreso">
+                    </div>
+
+
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Hora de Ingreso</label>
+                        <input type="text" class="form-control" id="recipient-name" placeholder="Hora de Ingreso">
+                    </div>
+
+
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Localidad</label>
+                        <input type="text" class="form-control" id="recipient-name" placeholder="Localidad">
+                    </div>
+
+                    </form>
                 </div>
-
-
-                <!--
-                <div class="mb-3">
-                    <label for="message-text" class="col-form-label">Message:</label>
-                    <textarea class="form-control" id="message-text"></textarea>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-primary">Cargar Asistencia</button>
                 </div>
-                -->
-
-                <div class="mb-3">
-                    <label for="recipient-name" class="col-form-label">Apellido</label>
-                    <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Apellido">
                 </div>
-
-
-                <div class="mb-3">
-                    <label for="recipient-name" class="col-form-label">Email</label>
-                    <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Email">
-                </div>
-
-
-                <div class="mb-3">
-                    <label for="recipient-name" class="col-form-label">Fecha</label>
-                    <input type="text" class="form-control" id="recipient-name" placeholder="Fecha de Ingreso">
-                </div>
-
-
-                <div class="mb-3">
-                    <label for="recipient-name" class="col-form-label">Hora de Ingreso</label>
-                    <input type="text" class="form-control" id="recipient-name" placeholder="Hora de Ingreso">
-                </div>
-
-
-                <div class="mb-3">
-                    <label for="recipient-name" class="col-form-label">Localidad</label>
-                    <input type="text" class="form-control" id="recipient-name" placeholder="Localidad">
-                </div>
-
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="button" class="btn btn-primary">Cargar Asistencia</button>
             </div>
             </div>
         </div>
-        </div>
-    </div>
 
 
 
