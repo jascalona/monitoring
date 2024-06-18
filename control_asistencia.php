eacd94fec682eeea2736af9bfff3489b6a41ac97
jaescalona <abrahamjose.1587@gmail.com>
2024-06-18 13:43:20 -0400

    new

--- control_asistencia.php
+++ control_asistencia.php
@@ -74,30 +74,71 @@ include "./CONTROLLER/conexion.php";
 
 
     <div class="carga-modal">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Carga de Asistencia</button>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Cargar Asistencia</button>
     
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Control de Asistencias</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form>
+                
                 <div class="mb-3">
-                    <label for="recipient-name" class="col-form-label">Recipient:</label>
-                    <input type="text" class="form-control" id="recipient-name">
+                    <label for="recipient-name" class="col-form-label">CI</label>
+                    <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Documento">
                 </div>
+
+
+                <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Nombre</label>
+                    <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Nombre">
+                </div>
+
+
+                <!--
                 <div class="mb-3">
                     <label for="message-text" class="col-form-label">Message:</label>
                     <textarea class="form-control" id="message-text"></textarea>
                 </div>
+                -->
+
+                <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Apellido</label>
+                    <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Apellido">
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Email</label>
+                    <input type="text" class="form-control" id="recipient-name" placeholder="Ingrese su Email">
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Fecha</label>
+                    <input type="text" class="form-control" id="recipient-name" placeholder="Fecha de Ingreso">
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Hora de Ingreso</label>
+                    <input type="text" class="form-control" id="recipient-name" placeholder="Hora de Ingreso">
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Localidad</label>
+                    <input type="text" class="form-control" id="recipient-name" placeholder="Localidad">
+                </div>
+
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Send message</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Cargar Asistencia</button>
             </div>
             </div>
         </div>
