da8adc344a369f5218632f70b6b2dddaac91d19e
jaescalona <abrahamjose.1587@gmail.com>
2024-06-20 12:01:42 -0400

    new

--- CONTROLLER/load.php
+++ CONTROLLER/load.php
@@ -1,18 +1,17 @@
 <?php
 
-if (!empty($_POST['btn-load'])) {
-    if (!empty($_POST['id']) and !empty($_POST['name']) and !empty($_POST['surname']) and !empty($_POST['ci']) and !empty($_POST['email']) and !empty($_POST['date']) and !empty($_POST['time'])){
-        
-        $id = $_POST['id'];
+if (!empty($_POST['btnCarga'])) {
+    if (!empty($_POST['name']) and !empty($_POST['surnmae']) and !empty($_POST['ci']) and !empty($_POST['email']) and !empty($_POST['date']) and !empty($_POST['time']) and !empty($_POST['location'])){
+
         $name = $_POST['name'];
         $surname = $_POST['surname'];
         $ci = $_POST['ci'];
         $email = $_POST['email'];
         $date = $_POST['date'];
-        $time = $_POST['time'];
-        $location = $_POST['location'];
+        $file = $_POST['time'];
 
-        $sql = $conexion->query(" insert into repositorio(id,name,surname,ci,email,date,time,location)values('$id','$name','$surname','$ci','$email','$date','$time','$location')");
+
+        $sql = $conexion->query(" insert into repositorio(name,surname,ci,email,date,time,location)values('$name','$surname','$ci','$email','$date', '$time', '$location')");
         if ($sql==1) {
             echo '<div style="margin-left: 470px; position: absolute;" class="alert alert-success">LA CARGA SE REALIZO CORRECTAMENTE</div>';
         } else {
@@ -20,7 +19,7 @@ if (!empty($_POST['btn-load'])) {
         }
         
     } else{
-        echo '<div style="margin-left: 500px; position: absolute;" class="alert alert-warning">DEBE LLENAR LOS CAMPOS VACIOS</div>';
+        echo '<div style="margin-left: 207px; position: absolute;" class="alert alert-warning">DEBE LLENAR LOS CAMPOS VACIOS</div>';
     }
 
 }
