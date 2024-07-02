eee6d9d75861645550bd7dc6b52bfc92174db5c6
jaescalona <abrahamjose.1587@gmail.com>
2024-07-02 10:37:47 -0400

    new

--- panel.php
+++ panel.php
@@ -146,7 +146,7 @@ if (!empty($_SESSION["CI"])) {
                   </p>
                 </i>  
 
-                <img class="img-fluid" src="https://images.unsplash.com/photo-1434494878577-86c23bcb06b9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                <img class="img-fluid" src="https://images.unsplash.com/photo-1489486501123-5c1af10d0be6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
               </div>
           </div>
 
@@ -241,7 +241,7 @@ if (!empty($_SESSION["CI"])) {
                   </p>
                 </i>  
 
-                <img class="img-fluid" src="https://images.unsplash.com/photo-1434494878577-86c23bcb06b9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                <img class="img-fluid" src="https://images.unsplash.com/photo-1489486501123-5c1af10d0be6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
               </div>
           </div>
 
